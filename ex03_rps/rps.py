826811a54d59100bfecb479361f932173b2847b3
Fawzy20 <fawzy-elsam@outlook.fr>
2021-09-23 12:01:17 +0200

    ajout de demande de reoujer

--- ex03_rps/rps.py
+++ ex03_rps/rps.py
@@ -29,19 +29,32 @@ def augmenter_scores(mon_coup, ton_coup):
         ton_score += 1
 
 def gagnant():
+
     if ton_score == fin:
         print("Vous avez gagner")
     elif mon_coup== fin:
         print("Vous avez perdut")
 
-while mon_score < fin and ton_score < fin:
-    
-    mon_coup = randint(1, 3)
-    ton_coup = int(input("1 : pierre, 2 : papier, 3 : ciseaux ? "))
-    while ton_coup < 1 or ton_coup > 3:
-        ton_coup = int(input("1 : pierre, 2 : papier, 3 : ciseaux ? "))
-    print("Vous montrez "+ str(ton_coup) + " il montre " + str(mon_coup), end="\n")
-    augmenter_scores(mon_coup, ton_coup)
-    print("mon score et de -> " + str(mon_score) + " son score et de -> " + str(ton_score), end="\n" )
+def rejouer():  
+        ton_choix = int(input("Voulez-vous rejouez ? 1 : oui, 2: non -> "))
+        if ton_choix == 1:
+            start()
+        else:
+            print("merci d'avoir jouer")       
 
-print(gagnant(), end="")
+def start():
+
+    while mon_score < fin and ton_score < fin:
+    
+        mon_coup = randint(1, 3)
+        ton_coup = int(input("1 : pierre, 2 : papier, 3 : ciseaux ? "))
+        while ton_coup < 1 or ton_coup > 3:
+            ton_coup = int(input("1 : pierre, 2 : papier, 3 : ciseaux ? "))
+        print("Vous montrez "+ str(ton_coup) + " il montre " + str(mon_coup), end="\n")
+        augmenter_scores(mon_coup, ton_coup)
+        print("mon score et de -> " + str(mon_score) + " son score et de -> " + str(ton_score), end="\n" )
+
+    print(gagnant(), end="")
+
+start()
+rejouer()
